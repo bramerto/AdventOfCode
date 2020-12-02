e508fc97065da250b4c7ed551c5c3d7e6529e87b
bramerto <bramerto@gmail.com>
2020-12-02 12:30:16 +0100

    Day 2 - puzzle 2

---
+++ 2020/Day_2/puzzle2.py
@@ -0,0 +1,17 @@
+with open("day2_input.txt", "r") as f:
+    passwordLines = map(str, f.readlines())
+
+amountValidated = 0
+
+for line in passwordLines:
+    [policy, password] = line.split(':')
+
+    [positions, char] = policy.split(" ")
+    [pos1, pos2] = list(map(int, positions.split("-")))
+
+    pArr = list(password)
+
+    if (pArr[pos1] == char and pArr[pos2] != char or pArr[pos1] != char and pArr[pos2] == char):
+        amountValidated += 1
+
+print(amountValidated)

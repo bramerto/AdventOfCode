1c61c1a0bc1e97061b5fe4bbdaab429401bc94b3
bramerto <bramerto@gmail.com>
2020-12-02 11:28:30 +0100

    Day 1 - puzzle 1

---
+++ 2020/Day_1/puzzle1.py
@@ -0,0 +1,12 @@
+with open("day1_input.txt", "r") as f:
+    numbers = list(map(int, f.readlines()))
+
+def getSumationOf2020():
+    for i in numbers:
+        if (i > 1000 and i < 2000):
+            for j in numbers:
+                if (i + j == 2020):
+                    print(i * j)
+                    return
+
+getSumationOf2020()
